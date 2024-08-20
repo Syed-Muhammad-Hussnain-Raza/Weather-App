304559aea6f96c95ed935ded1672cb34f526e6b6
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-20 12:49:42 +0500

    WeatherCard - Fetching code is defined here and use of different hooks also made for proper fetching of data

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+
+const API_KEY = "8840d1e8cfa1a535de3bcda96850af5e";
+
 const WeatherCard = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [search, setSearch] = useState(false);
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("Error while fetching weather data", error);
+      setWeatherData(null);
+      setSearch(true);
+    }
+  };
+
+  useEffect(() => {
+    if (city && search) {
+      fetchWeatherData();
+    }
+  }, [city, search]);
+
   return (
     <div>
       <h1>WeatherCard</h1>
