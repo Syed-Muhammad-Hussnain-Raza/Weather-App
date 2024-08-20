b0007bee06a3d8dad2ce6f0211fd8b22213946b0
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-20 14:24:38 +0500

    WeatherCard - UI for Sun Set and Rise define successfully

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { FaSearchLocation } from "react-icons/fa";
 import { BiWind } from "react-icons/bi";
 import { LuWaves } from "react-icons/lu";
+import { HiSun } from "react-icons/hi";
+import { PiCloudSunFill } from "react-icons/pi";
 
 const API_KEY = "8840d1e8cfa1a535de3bcda96850af5e";
 
@@ -65,6 +67,33 @@ const WeatherCard = () => {
                 <div className="flex flex-row items-center gap-x-3">
                   <LuWaves />
                   <p>{(weatherData?.main?.humidity).toFixed(2)} kmp</p>
+                </div>
+              </div>
+
+              <div className="flex flex-row justify-between mt-4 text-xl">
+                <div className="flex flex-row items-center gap-x-3">
+                  <HiSun />
+                  <p>
+                    {new Date(
+                      weatherData?.sys?.sunrise * 1000
+                    ).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      second: "2-digit",
+                    })}
+                  </p>
+                </div>
+                <div className="flex flex-row items-center gap-x-3">
+                  <PiCloudSunFill />
+                  <p>
+                    {new Date(
+                      weatherData?.sys?.sunset * 1000
+                    ).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      second: "2-digit",
+                    })}
+                  </p>
                 </div>
               </div>
             </div>
